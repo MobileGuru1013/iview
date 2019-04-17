215dbb95a53c1030b86c44fba89220e9a87ad413
Mobile Guru <alexandrlesan@yahoo.com>
2019-04-17 08:57:30 -0800

    iview
    A high quality UI Toolkit built on Vue.js 2.0 https://iviewui.com/

---
+++ examples/routers/tabs.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Button @click="showSecond = !showSecond">change</Button>
+        <Tabs value="name1">
+            <TabPane label="标签一" name="name1" :index="1">标签一的内容</TabPane>
+            <TabPane label="标签二" name="name2" :index="2" v-if="showSecond">标签二的内容</TabPane>
+            <TabPane label="标签三" name="name3" :index="3">标签三的内容</TabPane>
+        </Tabs>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                showSecond: false
+            }
+        }
+    }
+</script>
